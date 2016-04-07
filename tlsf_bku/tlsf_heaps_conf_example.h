7343aae633e0afcd94d6bddf21861b3d11377e2b
Alexandre Bustico <alexandre.bustico@enac.fr>
2016-04-07 14:57:47 +0200

    fix tlsf_heaps_conf_example.h

--- tlsf_bku/tlsf_heaps_conf_example.h
+++ tlsf_bku/tlsf_heaps_conf_example.h
@@ -1,5 +1,15 @@
+#if defined STM32F4XX
+#define NODMA_SECTION ".ram4"
+#define DMA_SECTION ".ram0"
+#elif  defined STM32F7XX
+#define NODMA_SECTION ".ram0"
+#define DMA_SECTION ".ram3"
+#else
+#error "section defined only for STM32F4 and STM32F7"
+#endif
+
 #define HEAP_CCM		ccmHeap
 #define HEAP_CCM_SIZE		16384
-#define HEAP_CCM_SECTION	".ccmram"
+#define HEAP_CCM_SECTION	NODMA_SECTION
 
 #define HEAP_DEFAULT		HEAP_CCM
