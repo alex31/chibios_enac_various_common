80e1b36c3672df301d2a1aaeba818b14435da988
Alexandre Bustico <alexandre.bustico@enac.fr>
2016-09-21 17:05:27 +0200

    use 32 bits time based timestamp to avoid replay attack

--- simpleSerialMessage.c
+++ simpleSerialMessage.c
@@ -3,8 +3,10 @@
 #include "globalVar.h"
 #include "stdutil.h"
 #include "simpleSerialMessage.h"
-#include <string.h>
+#include "rtcAccess.h"
 #include "mbedtls/aes.h"
+#include <string.h>
+
 
 #if (CH_KERNEL_MAJOR > 3)
 #define chSequentialStreamWrite  streamWrite
@@ -14,6 +16,7 @@
 
 #define MAX_CLEAR_LEN 255
 #define MAX_CYPHER_LEN 240
+#define NO_REPLAY_MAX_DRIFT 60
 
 static size_t padWithZeroes (uint8_t *input, const size_t inputSize,
 			     const size_t payloadLen, const size_t blockSize);
@@ -27,6 +30,9 @@ static size_t simpleMsgBufferDecapsulate (const uint8_t *inBuffer, uint8_t **pay
 
 static bool simpleMsgBufferDecypher (uint8_t *outBuffer, const uint8_t *inBuffer,
 				     const size_t outBufferSize, const size_t msgLen);
+static uint32_t getTimeHundredthOfSeconds(void);
+
+static void  initRtcWithCompileTime(void);
 
 typedef enum  {WAIT_FOR_SYNC, WAIT_FOR_LEN, WAIT_FOR_PAYLOAD, WAIT_FOR_CHECKSUM} SerialCmdState ;
 
@@ -54,13 +60,13 @@ typedef struct {
 
 typedef struct {
   struct {
-    uint16_t cnt;
+    time_t hos;
     uint8_t len;
     uint8_t payload[MAX_CLEAR_LEN]; // crc is add at end of payload
     // CRC is here after payload
   };
 } EncapsulatedFrame;
-#define ENCAP_OVH (sizeof(EncapsulatedFrame)-MAX_CLEAR_LEN)
+#define ENCAP_OVH ((sizeof(EncapsulatedFrame)-MAX_CLEAR_LEN)+2/*CRC*/)
 
 
 
@@ -83,7 +89,9 @@ bool simpleMsgCypherInit (const uint8_t *_key, const size_t keyLen, const uint8_
 {
   if ((keyLen != sizeof(key)) || (ivLen != sizeof(ivSource)))
     return false;
-      
+
+  initRtcWithCompileTime();
+  
   memcpy (key, _key, keyLen);
   memcpy (ivSource, iv, ivLen);
   mbedtls_aes_init (&aesEnc);
@@ -100,21 +108,16 @@ static bool simpleMsgBufferEncapsulate (uint8_t *outBuffer, const uint8_t *inBuf
 				 const size_t outBufferSize, const size_t payloadLen)
 {
   EncapsulatedFrame *ec = (EncapsulatedFrame *) outBuffer;
-  static uint16_t count=0;
-
-  if ((payloadLen+ENCAP_OVH) > outBufferSize)
-    return false;
-
-  // zero is special meaning : reserved for "not yet receive message", should never be issued
-  if (++count == 0)
-    count++;
   
-  ec->cnt = count;
+  if ((payloadLen+ENCAP_OVH) > outBufferSize) {
+    return false;
+  }
+  ec->hos = getTimeHundredthOfSeconds();
   ec->len = payloadLen;
   memcpy (ec->payload, inBuffer, payloadLen);
   // put the crc after payload
   *((uint16_t *) (&ec->payload[payloadLen])) = fletcher16WithLen (ec->payload, payloadLen);
-
+  
   return true;
 }
 
@@ -124,9 +127,11 @@ static size_t simpleMsgBufferCypher (uint8_t *outBuffer, const uint8_t *inBuffer
   uint8_t iv[sizeof(ivSource)];
 
   const size_t paddedLen = padWithZeroes (outBuffer, outBufferSize, payloadLen, 16);
-  if (paddedLen > outBufferSize) 
+  if (paddedLen == 0) {
+    DebugTrace ("cypher ERROR : payloadLen[%d] paddedLen[%d] > outBufferSize[%d]",
+		payloadLen, paddedLen, outBufferSize);
     return 0;
-
+  }
   memcpy (iv, ivSource, sizeof(iv));
   mbedtls_aes_crypt_cbc (&aesEnc, MBEDTLS_AES_ENCRYPT, paddedLen, iv, inBuffer, outBuffer);
   return paddedLen;
@@ -135,8 +140,7 @@ static size_t simpleMsgBufferCypher (uint8_t *outBuffer, const uint8_t *inBuffer
 size_t simpleMsgBufferEncapsulateAndCypher (uint8_t *outBuffer, const uint8_t *inBuffer,
 					  const size_t outBufferSize, const size_t payloadLen)
 {
-  uint8_t encapMsg[payloadLen+ENCAP_OVH];
-  
+ uint8_t encapMsg[payloadLen+ENCAP_OVH];
   if (!simpleMsgBufferEncapsulate (encapMsg, inBuffer, payloadLen+ENCAP_OVH, payloadLen))
     return 0;
   return simpleMsgBufferCypher (outBuffer, encapMsg, outBufferSize, payloadLen+ENCAP_OVH);
@@ -155,8 +159,9 @@ bool simpleMsgSend (BaseSequentialStream * const channel, uint8_t *inBuffer,
   if (doCypher) {
     len = simpleMsgBufferEncapsulateAndCypher (cypherBuffer, inBuffer, slen+ENCAP_OVH+16, slen);
     
-    if (len == 0)
+    if (len == 0) {
       return false;
+    }
   }
   
   const MsgHeader msgHeader = {.sync = 0x99,
@@ -209,31 +214,34 @@ Thread * simpleMsgBind (BaseSequentialStream *channel, const MsgCallBack callbac
   return tp;
 }
 
-static inline uint16_t udiff (const uint16_t a, const uint16_t b) {
-  return (b>a) ? b-a : (65535-a)+b;
-}
+/* static inline uint16_t udiff (const uint16_t a, const uint16_t b) { */
+/*   return (b>a) ? b-a : (65535-a)+b; */
+/* } */
 
 static size_t simpleMsgBufferDecapsulate (const uint8_t *inBuffer, uint8_t **payload)
 {
   EncapsulatedFrame *ec = (EncapsulatedFrame *) inBuffer;
-  static uint16_t count=0; // special value : not yet receive a message
+  static time_t lastClock=0;
   
   uint16_t crc =  fletcher16WithLen (ec->payload, ec->len);
-  // test aigainst embedded crc which is after payload
+  // test against embedded crc which is after payload
   if (crc !=   *((uint16_t *) (&ec->payload[ec->len]))) {
     DebugTrace ("decypher CRC error");
     goto fail;
   }
   
   // we trust the first message
-  if (!count) {
-    count = ec->cnt;
-  } else if ((count == ec->cnt) || (udiff (count, ec->cnt) > 10000)) {
-    DebugTrace ("Replay Detected local=%d cnt=%d diff=%d", count, ec->cnt, udiff (ec->cnt, count));
+  if (lastClock == 0) {
+    lastClock = ec->hos;
+    // received clock has to grow forever (2^32 hundreds of seconds = 497 days max)
+    // if we want to flight more, we have to manage clock wrapping
+  } else if (ec->hos <= lastClock) {
+    DebugTrace ("Replay Detected");
     goto fail;
+  } else {
+    lastClock = ec->hos;
   }
-  
-  count = ec->cnt;
+
   *payload = ec->payload;
   return ec->len;
   
@@ -314,7 +322,7 @@ static void readAndProcessChannel(void *arg)
     switch (messState.state) {
       
     case WAIT_FOR_SYNC :
-      // DebugTrace ("WAIT_FOR_SYNC");
+      //DebugTrace ("WAIT_FOR_SYNC");
       messState.payload[0] = (uint8_t) chSequentialStreamGet (mbp->channel);
       /* if ( (*((uint16_t *) &messState.payload[0]) & 0xffff) == 0xFEED) { */
       /* 	messState.state = WAIT_FOR_LEN; */
@@ -325,20 +333,20 @@ static void readAndProcessChannel(void *arg)
       break;
       
     case WAIT_FOR_LEN :
-      // DebugTrace ("WAIT_FOR_LEN");
+      //DebugTrace ("WAIT_FOR_LEN");
       messState.len = (uint8_t) chSequentialStreamGet (mbp->channel);
       // DebugTrace ("LEN = %d", messState.len);
       messState.state = WAIT_FOR_PAYLOAD;
       break;
 
     case WAIT_FOR_PAYLOAD :
-      // DebugTrace ("WAIT_FOR_PAYLOAD");
+      //DebugTrace ("WAIT_FOR_PAYLOAD");
       chSequentialStreamRead (mbp->channel, messState.payload,  messState.len);
       messState.state = WAIT_FOR_CHECKSUM;
       break;
 
     case WAIT_FOR_CHECKSUM :
-      // DebugTrace ("WAIT_FOR_CHECKSUM");
+      //DebugTrace ("WAIT_FOR_CHECKSUM");
       chSequentialStreamRead (mbp->channel, (uint8_t *) &messState.crc,  sizeof(messState.crc));
       const uint16_t calculatedCrc = fletcher16WithLen (messState.payload, messState.len);
       if (calculatedCrc == messState.crc) {
@@ -355,8 +363,8 @@ static void readAndProcessChannel(void *arg)
 	  mbp->callback (messState.payload, messState.len, mbp->userData);
 	}
       } else {
-	// DebugTrace ("CRC ERROR : calculated 0x%x != in message 0x%x", calculatedCrc, 
-	//	    messState.crc);
+	 DebugTrace ("CRC ERROR : calculated 0x%x != in message 0x%x", calculatedCrc, 
+		     messState.crc);
       }
       messState.state = WAIT_FOR_SYNC;
       break;
@@ -382,7 +390,28 @@ static size_t padWithZeroes (uint8_t *input, const size_t inputSize,
     }
     return payloadLen + padSize;
   } else {
+    DebugTrace ("PWZ error payloadLen[%d] + padSize[%d] > inputSize[%d]",
+		payloadLen, padSize,  inputSize); 
     return 0;
   }
 }
 
+static uint32_t getTimeHundredthOfSeconds()
+{
+  /* DebugTrace ("getTimeUnixMilliSec() - (EPOCHTS*1000) = %d", */
+  /* 	      (uint32_t) (getTimeUnixMillisec() - ((uint64_t)(EPOCHTS)*1000ULL) / 10ULL)); */
+  
+   return ((getTimeUnixMillisec() - (EPOCHTS*1000ULL)) / 10ULL);
+}
+
+static void  initRtcWithCompileTime(void)
+{
+  // if the RTC date is less than date of compilation, obviously RTC is not accurate
+  // so let set it to date of compilation which is less false RTC
+  // since we sent RTC-EPOCHTS as anti replay mecanism, RTC-EPOCHTS should alway be positive
+  // if afterward RTC is set to accurate (GPS) time, it will go further forward and that will
+  // not affect anti replay algorithm
+  if (getTimeUnixSec() < EPOCHTS) {
+    setTimeUnixSec (EPOCHTS);
+  }
+}
