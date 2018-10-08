d175cdf23989e0c737df42968ec6984fccc6474d
Alexandre Bustico <alexandre.bustico@enac.fr>
2018-10-08 21:00:17 +0200

    append chibios .mk file

--- serial_message/serial_message.mk
+++ serial_message/serial_message.mk
@@ -2,3 +2,6 @@ VARIOUS_CPPSRC += $(VARIOUS)/serial_message/serialMsg.cpp \
 		  $(VARIOUS)/serial_message/libsrc/serialMsg_conf_lib.cpp
 
 VARIOUS_INCL += $(VARIOUS)/serial_message
+
+VARIOUS_CSRC  +=      $(VARIOUS)/ringBuffer.c \
+		      $(VARIOUS)/pipe.c 
