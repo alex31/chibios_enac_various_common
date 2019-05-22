c98dfdbc8eab2b5546ae18318cd3429bb90ec1e3
Alexandre Bustico <alexandre.bustico@enac.fr>
2019-05-22 18:33:18 +0200

    add HD44780 LCD display driver

---
+++ hd44780.c
@@ -0,0 +1,535 @@
+/*
+    PLAY Embedded - Copyright (C) 2006..2015 Rocco Marco Guglielmi
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/*
+    Special thanks to Giovanni Di Sirio for teachings, his moral support and
+    friendship. Note that some or every piece of this file could be part of
+    the ChibiOS project that is intellectual property of Giovanni Di Sirio.
+    Please refer to ChibiOS/RT license before use this file.
+	
+	For suggestion or Bug report - guglielmir@playembedded.org
+ */
+
+/**
+ * @file    hd44780.c
+ * @brief   HD44780 complex driver code.
+ *
+ * @addtogroup HD44780
+ * @{
+ */
+
+#include "hd44780.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+#if USERLIB_USE_HD44780 || defined(__DOXYGEN__)
+
+/*===========================================================================*/
+/* Driver local definitions.                                                 */
+/*===========================================================================*/
+
+
+/* HD44780 REGISTERS */
+#define HD44780_INSTRUCTION_R               PAL_LOW
+#define HD44780_DATA_R                      PAL_HIGH
+
+/* HD44780_INSTRUCTIONS */
+#define HD44780_CLEAR_DISPLAY               0x01
+
+#define HD44780_RETURN_HOME                 0x02
+
+#define HD44780_EMS                         0x04
+#define HD44780_EMS_S                       0x01
+#define HD44780_EMS_ID                      0x02
+
+#define HD44780_DC                          0x08
+#define HD44780_DC_B                        0x01
+#define HD44780_DC_C                        0x02
+#define HD44780_DC_D                        0x04
+
+#define HD44780_CDS                         0x10
+#define HD44780_CDS_RL                      0x04
+#define HD44780_CDS_SC                      0x08
+
+#define HD44780_FS                          0x20
+#define HD44780_FS_F                        0x04
+#define HD44780_FS_N                        0x08
+#define HD44780_FS_DL                       0x10
+
+#define HD44780_SET_CGRAM_ADDRESS           0x40
+#define HD44780_SET_CGRAM_ADDRESS_MASK      0X3F
+
+#define HD44780_SET_DDRAM_ADDRESS           0x80
+#define HD44780_SET_DDRAM_ADDRESS_MASK      0X7F
+
+#define HD44780_BUSY_FLAG                   0X80
+
+/*===========================================================================*/
+/* Driver exported variables.                                                */
+/*===========================================================================*/
+
+/**
+ * @brief   HD44780D1 driver identifier.
+ */
+HD44780Driver HD44780D1;
+
+/*===========================================================================*/
+/* Driver local types.                                                       */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver local variables.                                                   */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver local functions.                                                   */
+/*===========================================================================*/
+
+/**
+ * @brief   Get the busy flag
+ *
+ * @param[in] hd44780p          HD44780 driver
+ *
+ * @return                  The HD44780 status.
+ * @retval TRUE             if the HD44780 is busy on internal operation.
+ * @retval FALSE            if the HD44780 is in idle.
+ *
+ * @notapi
+ */
+static bool hd44780IsBusy(HD44780Driver *lcdp) {
+  bool busy;
+  unsigned ii;
+
+  /* Configuring Data PINs as Input. */
+  for(ii = 0; ii < LINE_DATA_LEN; ii++)
+    palSetLineMode(lcdp->config->pinmap->D[ii], PAL_MODE_INPUT);
+
+  palSetLine(lcdp->config->pinmap->RW);
+  palClearLine(lcdp->config->pinmap->RS);
+
+  palSetLine(lcdp->config->pinmap->E);
+  osalThreadSleepMicroseconds(100);
+  busy = (palReadLine(lcdp->config->pinmap->D[LINE_DATA_LEN - 1]) == PAL_HIGH);
+  palClearLine(lcdp->config->pinmap->E);
+  osalThreadSleepMicroseconds(100);
+
+#if HD44780_USE_4_BIT_MODE
+  palSetLine(lcdp->config->pinmap->E);
+  osalThreadSleepMicroseconds(100);
+  palClearLine(lcdp->config->pinmap->E);
+  osalThreadSleepMicroseconds(100);
+#endif
+  return busy;
+}
+
+/**
+ * @brief   Write a data into a register for the lcd
+ *
+ * @param[in] lcdp          HD44780 driver
+ * @param[in] reg           Register id
+ * @param[in] value         Writing value
+ *
+ * @notapi
+ */
+static void hd44780WriteRegister(HD44780Driver *lcdp, uint8_t reg, uint8_t value){
+
+  unsigned ii;
+
+  while (hd44780IsBusy(lcdp))
+    ;
+
+  /* Configuring Data PINs as Output Push Pull. */
+  for(ii = 0; ii < LINE_DATA_LEN; ii++)
+    palSetLineMode(lcdp->config->pinmap->D[ii], PAL_MODE_OUTPUT_PUSHPULL |
+                   PAL_STM32_OSPEED_HIGHEST);
+
+  palClearLine(lcdp->config->pinmap->RW);
+  palWriteLine(lcdp->config->pinmap->RS, reg);
+
+#if HD44780_USE_4_BIT_MODE
+  for(ii = 0; ii < LINE_DATA_LEN; ii++) {
+    if(value & (1 << (ii + 4)))
+      palSetLine(lcdp->config->pinmap->D[ii]);
+    else
+      palClearLine(lcdp->config->pinmap->D[ii]);
+  }
+  palSetLine(lcdp->config->pinmap->E);
+  osalThreadSleepMicroseconds(100);
+  palClearLine(lcdp->config->pinmap->E);
+  osalThreadSleepMicroseconds(100);
+
+  for(ii = 0; ii < LINE_DATA_LEN; ii++) {
+    if(value & (1 << ii))
+      palSetLine(lcdp->config->pinmap->D[ii]);
+    else
+      palClearLine(lcdp->config->pinmap->D[ii]);
+  }
+  palSetLine(lcdp->config->pinmap->E);
+  osalThreadSleepMicroseconds(100);
+  palClearLine(lcdp->config->pinmap->E);
+  osalThreadSleepMicroseconds(100);
+#else
+  for(ii = 0; ii < LINE_DATA_LEN; ii++){
+      if(value & (1 << ii))
+        palSetLine(lcdp->config->pinmap->D[ii]);
+      else
+        palClearLine(lcdp->config->pinmap->D[ii]);
+  }
+  palSetLine(lcdp->config->pinmap->E);
+  osalThreadSleepMicroseconds(100);
+  palClearLine(lcdp->config->pinmap->E);
+  osalThreadSleepMicroseconds(100);
+#endif
+}
+
+/**
+ * @brief   Perform a initialization by instruction as explained in HD44780
+ *          datasheet.
+ * @note    This reset is required after a mis-configuration or if there aren't
+ *          condition to enable internal reset circuit.
+ *
+ * @param[in] lcdp          HD44780 driver
+ *
+ * @notapi
+ */
+static void hd44780InitByIstructions(HD44780Driver *lcdp) {
+  unsigned ii;
+
+  osalThreadSleepMilliseconds(50);
+  for(ii = 0; ii < LINE_DATA_LEN; ii++) {
+    palSetLineMode(lcdp->config->pinmap->D[ii], PAL_MODE_OUTPUT_PUSHPULL |
+                   PAL_STM32_OSPEED_HIGHEST);
+    palClearLine(lcdp->config->pinmap->D[ii]);
+  }
+
+  palClearLine(lcdp->config->pinmap->E);
+  palClearLine(lcdp->config->pinmap->RW);
+  palClearLine(lcdp->config->pinmap->RS);
+  palSetLine(lcdp->config->pinmap->D[LINE_DATA_LEN - 3]);
+  palSetLine(lcdp->config->pinmap->D[LINE_DATA_LEN - 4]);
+
+  palSetLine(lcdp->config->pinmap->E);
+  osalThreadSleepMicroseconds(100);
+  palClearLine(lcdp->config->pinmap->E);
+  osalThreadSleepMilliseconds(5);
+
+  palSetLine(lcdp->config->pinmap->E);
+  osalThreadSleepMicroseconds(100);
+  palClearLine(lcdp->config->pinmap->E);
+  osalThreadSleepMicroseconds(100);
+
+  palSetLine(lcdp->config->pinmap->E);
+  osalThreadSleepMicroseconds(100);
+  palClearLine(lcdp->config->pinmap->E);
+
+#if HD44780_USE_4_BIT_MODE
+  palSetLine(lcdp->config->pinmap->D[LINE_DATA_LEN - 3]);
+  palClearLine(lcdp->config->pinmap->D[LINE_DATA_LEN - 4]);
+  palSetLine(lcdp->config->pinmap->E);
+  osalThreadSleepMicroseconds(100);
+  palClearLine(lcdp->config->pinmap->E);
+#endif
+
+  /* Configuring data interface */
+  hd44780WriteRegister(lcdp, HD44780_INSTRUCTION_R, HD44780_FS | HD44780_DATA_LENGHT |
+                       lcdp->config->font | lcdp->config->lines);
+
+  /* Turning off display and clearing */
+  hd44780WriteRegister(lcdp, HD44780_INSTRUCTION_R, HD44780_DC);
+  hd44780WriteRegister(lcdp, HD44780_INSTRUCTION_R, HD44780_CLEAR_DISPLAY);
+
+  /* Setting display control turning on display */
+  hd44780WriteRegister(lcdp, HD44780_INSTRUCTION_R, HD44780_DC | HD44780_DC_D |
+                       lcdp->config->cursor | lcdp->config->blinking);
+
+  /* Setting Entry Mode */
+  hd44780WriteRegister(lcdp, HD44780_INSTRUCTION_R, HD44780_EMS | HD44780_EMS_ID);
+}
+
+/*===========================================================================*/
+/* Driver exported functions.                                                */
+/*===========================================================================*/
+
+
+/**
+ * @brief   Initializes an instance.
+ *
+ * @param[out] lcdp         pointer to the @p HD44780Driver object
+ *
+ * @init
+ */
+void hd44780ObjectInit(HD44780Driver *lcdp){
+
+  lcdp->state  = HD44780_STOP;
+  lcdp->config = NULL;
+  lcdp->backlight = 0;
+}
+
+/**
+ * @brief   Configures and activates the HD44780 Complex Driver  peripheral.
+ *
+ * @param[in] lcd   pointer to the @p HD44780Driver object
+ * @param[in] config    pointer to the @p HD44780Config object
+ *
+ * @api
+ */
+void hd44780Start(HD44780Driver *lcdp, const HD44780Config *config) {
+
+  osalDbgCheck((lcdp != NULL) && (config != NULL));
+
+  osalDbgAssert((lcdp->state == HD44780_STOP) || (lcdp->state == HD44780_ACTIVE),
+              "hd44780Start(), invalid state");
+
+  lcdp->config = config;
+  lcdp->backlight = lcdp->config->backlight;
+
+  /* Initializing HD44780 by instructions. */
+  hd44780InitByIstructions(lcdp);
+
+#if HD44780_USE_DIMMABLE_BACKLIGHT
+  pwmStart(lcdp->config->pwmp, lcdp->config->pwmcfgp);
+  pwmEnableChannel(lcdp->config->pwmp, lcdp->config->channelid,
+                   PWM_PERCENTAGE_TO_WIDTH(lcdp->config->pwmp,
+                                           lcdp->config->backlight * 100));
+
+#else
+  palWriteLine(lcdp->config->pinmap->A,
+              lcdp->config->backlight ? PAL_HIGH : PAL_LOW);
+#endif
+
+  lcdp->state = HD44780_ACTIVE;
+}
+
+/**
+ * @brief   Deactivates the HD44780 Complex Driver  peripheral.
+ *
+ * @param[in] lcdp      pointer to the @p HD44780Driver object
+ *
+ * @api
+ */
+void hd44780Stop(HD44780Driver *lcdp) {
+
+  osalDbgCheck(lcdp != NULL);
+
+  osalDbgAssert((lcdp->state == HD44780_STOP) || (lcdp->state == HD44780_ACTIVE),
+              "lcdStop(), invalid state");
+#if HD44780_USE_DIMMABLE_BACKLIGHT
+  pwmStop(lcdp->config->pwmp);
+#else
+  palClearLine(lcdp->config->pinmap->A);
+#endif
+  lcdp->backlight = 0;
+  hd44780WriteRegister(lcdp, HD44780_INSTRUCTION_R, HD44780_DC);
+  hd44780WriteRegister(lcdp, HD44780_INSTRUCTION_R, HD44780_CLEAR_DISPLAY);
+  lcdp->state = HD44780_STOP;
+}
+
+/**
+ * @brief   Turn on back-light.
+ *
+ * @param[in] lcdp      pointer to the @p HD44780Driver object
+ *
+ * @api
+ */
+void hd44780BacklightOn(HD44780Driver *lcdp) {
+
+  osalDbgCheck(lcdp != NULL);
+  osalDbgAssert((lcdp->state == HD44780_ACTIVE), "lcdBacklightOn(), invalid state");
+#if HD44780_USE_DIMMABLE_BACKLIGHT
+  pwmEnableChannel(lcdp->config->pwmp, lcdp->config->channelid,
+                   PWM_PERCENTAGE_TO_WIDTH(lcdp->config->pwmp, 10000));
+
+#else
+  palSetLine(lcdp->config->pinmap->A);
+#endif
+  lcdp->backlight = 100;
+}
+
+/**
+ * @brief   Turn off back-light.
+ *
+ * @param[in] lcdp      pointer to the @p HD44780Driver object
+ *
+ * @api
+ */
+void hd44780BacklightOff(HD44780Driver *lcdp) {
+
+  osalDbgCheck(lcdp != NULL);
+  osalDbgAssert((lcdp->state == HD44780_ACTIVE), "lcdBacklightOff(), invalid state");
+#if HD44780_USE_DIMMABLE_BACKLIGHT
+  pwmDisableChannel(lcdp->config->pwmp, lcdp->config->channelid);
+
+#else
+  palClearLine(lcdp->config->pinmap->A);
+#endif
+  lcdp->backlight = 0;
+}
+
+/**
+ * @brief   Clears display and return cursor in the first position.
+ *
+ * @param[in] lcdp      pointer to the @p HD44780Driver object
+ *
+ * @api
+ */
+void hd44780ClearDisplay(HD44780Driver *lcdp){
+
+  osalDbgCheck(lcdp != NULL);
+  osalDbgAssert((lcdp->state == HD44780_ACTIVE), "lcdClearDisplay(), invalid state");
+  hd44780WriteRegister(lcdp, HD44780_INSTRUCTION_R, HD44780_CLEAR_DISPLAY);
+}
+
+/**
+ * @brief   Return cursor in the first position.
+ *
+ * @param[in] lcdp      pointer to the @p HD44780Driver object
+ *
+ * @api
+ */
+void hd44780ReturnHome(HD44780Driver *lcdp){
+
+  osalDbgCheck(lcdp != NULL);
+  osalDbgAssert((lcdp->state == HD44780_ACTIVE), "lcdReturnHome(), invalid state");
+  hd44780WriteRegister(lcdp, HD44780_INSTRUCTION_R, HD44780_RETURN_HOME);
+}
+
+/**
+ * @brief   Set DDRAM address position leaving data unchanged.
+ *
+ * @param[in] lcdp      pointer to the @p HD44780Driver object
+ * @param[in] add       DDRAM address (from 0 to HD44780_DDRAM_MAX_ADDRESS)
+ *
+ * @api
+ */
+void hd44780SetAddress(HD44780Driver *lcdp, uint8_t add){
+
+  osalDbgCheck(lcdp != NULL);
+  osalDbgAssert((lcdp->state == HD44780_ACTIVE),
+                "lcdSetAddress(), invalid state");
+  if(add > HD44780_SET_DDRAM_ADDRESS_MASK)
+    return;
+  hd44780WriteRegister(lcdp, HD44780_INSTRUCTION_R, HD44780_SET_DDRAM_ADDRESS | add);
+}
+
+/**
+ * @brief   Writes string starting from a certain position.
+ *
+ * @detail  If string lenght exceeds, then is cutted
+ *
+ * @param[in] lcdp      pointer to the @p HD44780Driver object
+ * @param[in] string    string to write
+ * @param[in] pos       position for cursor (from 0 to HD44780_DDRAM_MAX_ADDRESS)
+ *
+ * @api
+ */
+void hd44780WriteString(HD44780Driver *lcdp, uint8_t pos, const char* fmt, ...){
+  va_list ap;
+  char string[80];
+  const char *s = string;
+  osalDbgCheck((lcdp != NULL) && (string != NULL));
+  osalDbgAssert((lcdp->state == HD44780_ACTIVE),
+                "lcdWriteString(), invalid state");
+
+  va_start(ap, fmt);
+  vsnprintf(string, sizeof(string), fmt, ap);
+  va_end(ap);
+  
+  int32_t  iteration = HD44780_SET_DDRAM_ADDRESS_MASK - pos + 1;
+  if(iteration > 0){
+    hd44780SetAddress(lcdp, pos);
+    while((*s != '\0') && (iteration > 0)){
+      hd44780WriteRegister(lcdp, HD44780_DATA_R, *s);
+      s++;
+      iteration--;
+    }
+  }
+}
+
+/**
+ * @brief   Makes a shift according to an arbitrary direction
+ *
+ * @param[in] lcdp      pointer to the @p HD44780Driver object
+ * @param[in] dir       direction (HD44780_RIGHT or HD44780_LEFT)
+ *
+ * @api
+ */
+void hd44780DoDisplayShift(HD44780Driver *lcdp, uint8_t dir){
+
+  osalDbgCheck(lcdp != NULL);
+  osalDbgAssert((lcdp->state == HD44780_ACTIVE),
+                "lcdDoDisplayShift(), invalid state");
+  hd44780WriteRegister(lcdp, HD44780_INSTRUCTION_R, HD44780_CDS | HD44780_CDS_SC | dir);
+}
+
+#if HD44780_USE_DIMMABLE_BACKLIGHT
+/**
+ * @brief   Set back-light percentage.
+ *
+ * @param[in] lcdp      pointer to the @p HD44780Driver object
+ * @param[in] perc      back-light percentage (from 0 to 100)
+ *
+ * @api
+ */
+void hd44780SetBacklight(HD44780Driver *lcdp, uint32_t perc){
+
+  osalDbgCheck(lcdp != NULL);
+  osalDbgAssert((lcdp->state == HD44780_ACTIVE), "lcdSetBacklight(), invalid state");
+  pwmEnableChannel(lcdp->config->pwmp, lcdp->config->channelid,
+                   PWM_PERCENTAGE_TO_WIDTH(lcdp->config->pwmp, perc * 100));
+  lcdp->backlight = perc;
+}
+
+/**
+ * @brief   Shift back-light from current value to 0.
+ *
+ * @param[in] lcdp      pointer to the @p HD44780Driver object
+ *
+ * @api
+ */
+void hd44780BacklightFadeOut(HD44780Driver *lcdp){
+  uint32_t curr = lcdp->backlight;
+  osalDbgCheck(lcdp != NULL);
+  osalDbgAssert((lcdp->state == HD44780_ACTIVE),
+                "lcdBacklightFadeOut(), invalid state");
+  while(curr != 0){
+    curr--;
+    hd44780SetBacklight(lcdp, curr);
+    osalThreadSleepMilliseconds(10);
+  }
+}
+
+/**
+ * @brief   Shift back-light from current value to 100.
+ *
+ * @param[in] lcdp      pointer to the @p HD44780Driver object
+ *
+ * @api
+ */
+void hd44780BacklightFadeIn(HD44780Driver *lcdp){
+  uint32_t curr = lcdp->backlight;
+  osalDbgCheck(lcdp != NULL);
+  osalDbgAssert((lcdp->state == HD44780_ACTIVE),
+                "lcdBacklightFadeIn(), invalid state");
+  while(curr != 100){
+    curr++;
+    hd44780SetBacklight(lcdp, curr);
+    osalThreadSleepMilliseconds(10);
+  }
+}
+#endif
+
+/** @} */
+#endif /* USERLIB_USE_HD44780 */
---
+++ hd44780.h
@@ -0,0 +1,272 @@
+/*
+    PLAY Embedded - Copyright (C) 2006..2015 Rocco Marco Guglielmi
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/*
+    Special thanks to Giovanni Di Sirio for teachings, his moral support and
+    friendship. Note that some or every piece of this file could be part of
+    the ChibiOS project that is intellectual property of Giovanni Di Sirio.
+    Please refer to ChibiOS/RT license before use this file.
+	
+	For suggestion or Bug report - guglielmir@playembedded.org
+ */
+
+/**
+ * @file    hd44780.h
+ * @brief   HD44780 Complex Driver header.
+ *
+ * @addtogroup HD44780
+ * @{
+ */
+
+#pragma once
+
+#include "hal.h"
+
+#if USERLIB_USE_HD44780 || defined(__DOXYGEN__)
+
+/*===========================================================================*/
+/* Driver constants.                                                         */
+/*===========================================================================*/
+
+
+/**
+ * @brief   Generic definition of right direction
+ */
+#define   HD44780_RIGHT                     0x04
+
+/**
+ * @brief   Generic definition of left direction
+ */
+#define   HD44780_LEFT                      0x00
+
+/*===========================================================================*/
+/* Driver pre-compile time settings.                                         */
+/*===========================================================================*/
+
+/**
+ * @brief   Enables back-light APIs.
+ *
+ * @note    Enabling this option HD44780 requires a PWM driver.
+ * @note    The default is @p TRUE.
+ */
+#if !defined(HD44780_USE_DIMMABLE_BACKLIGHT) || defined(__DOXYGEN__)
+#define HD44780_USE_DIMMABLE_BACKLIGHT               TRUE
+#endif
+
+/**
+ * @brief   Enables 4 BIT mode.
+ *
+ * @note    Enabling this option HD44780 uses only D4 to D7 pins
+ * @note    The default is @p FALSE.
+ */
+#if !defined(HD44780_USE_4_BIT_MODE) || defined(__DOXYGEN__)
+#define HD44780_USE_4_BIT_MODE              FALSE
+#endif
+
+/*===========================================================================*/
+/* Derived constants and error checks.                                       */
+/*===========================================================================*/
+
+#if HD44780_USE_4_BIT_MODE
+#define LINE_DATA_LEN                   4
+#define HD44780_DATA_LENGHT                 0x00
+#else
+#define LINE_DATA_LEN                   8
+#define HD44780_DATA_LENGHT                 0x10
+#endif
+
+#if HD44780_USE_DIMMABLE_BACKLIGHT && !HAL_USE_PWM
+#error "HD44780_USE_DIMMABLE_BACKLIGHT requires HAL_USE_PWM"
+#endif
+
+/*===========================================================================*/
+/* Driver data structures and types.                                         */
+/*===========================================================================*/
+
+/**
+ * @name    hd44780 data structures and types
+ * @{ */
+
+/**
+ * @brief   hd44780 PIN-map.
+ * @note    these PINs are required by write/read operation.
+ */
+typedef struct {
+  /**
+   * @brief  Register selector PIN
+   */
+  ioline_t RS;
+  /**
+   * @brief  Read/Write PIN
+   */
+  ioline_t RW;
+  /**
+   * @brief  Enable PIN
+   */
+  ioline_t E;
+  /**
+   * @brief  Back-light anode pin
+   */
+  ioline_t A;
+  /**
+   * @brief  Data PINs
+   */
+  ioline_t D[LINE_DATA_LEN];
+} hd44780_pins_t;
+
+/**
+ * @brief  HD44780 cursor control
+ */
+typedef enum {
+  HD44780_CURSOR_OFF = 0x00,
+  HD44780_CURSOR_ON = 0x02,
+} hd44780_cursor_t;
+
+/**
+ * @brief  HD44780 blinking control
+ */
+typedef enum {
+  HD44780_BLINKING_OFF = 0x00,
+  HD44780_BLINKING_ON = 0x01,
+} hd44780_blinking_t;
+
+/**
+ * @brief  HD44780 display settings
+ */
+typedef enum {
+  HD44780_SET_FONT_5X8 = 0x00,
+  HD44780_SET_FONT_5X10 = 0x04
+} hd44780_set_font_t;
+
+/**
+ * @brief  HD44780 display settings
+ */
+typedef enum {
+  HD44780_SET_1LINE = 0x00,
+  HD44780_SET_2LINES = 0x08
+} hd44780_set_lines_t;
+/** @}  */
+
+/**
+ * @brief   Driver state machine possible states.
+ */
+typedef enum {
+  HD44780_UNINIT = 0,
+  HD44780_STOP = 1,
+  HD44780_ACTIVE = 2,
+} hd44780_state_t;
+
+/**
+ * @brief   HD44780 configuration structure.
+ */
+typedef struct {
+  /**
+   * @brief  HD44780 cursor control
+   */
+  hd44780_cursor_t cursor;
+  /**
+   * @brief  HD44780 blinking control
+   */
+  hd44780_blinking_t blinking;
+  /**
+   * @brief  HD44780 font settings
+   */
+  hd44780_set_font_t font;
+  /**
+   * @brief  HD44780 lines settings
+   */
+  hd44780_set_lines_t lines;
+  /**
+   * @brief  HD44780 PIN-map
+   */
+  hd44780_pins_t const *pinmap;
+#if HD44780_USE_DIMMABLE_BACKLIGHT
+  /**
+   * @brief  PWM driver for back-light managing
+   */
+  PWMDriver *pwmp;
+  /**
+   * @brief  PWM driver configuration
+   */
+  PWMConfig const *pwmcfgp;
+  /**
+   * @brief  PWM channel id
+   */
+  uint32_t channelid;
+#endif
+  /**
+   * @brief  Initial Back-light percentage (from 0 to 100)
+   * @note   If !HD44780_USE_DIMMABLE_BACKLIGHT this is just true or false
+   */
+  uint32_t backlight;
+} HD44780Config;
+
+/**
+ * @brief   Structure representing an HD44780 driver.
+ */
+typedef struct {
+  /**
+   * @brief Driver state.
+   */
+  hd44780_state_t         state;
+  /**
+   * @brief  Current Back-light percentage (from 0 to 100)
+   *
+   * @detail When HD44780_USE_DIMMABLE_BACKLIGHT is false, this is considered like boolean
+   */
+  uint32_t           backlight;
+  /**
+   * @brief Current configuration data.
+   */
+  const HD44780Config    *config;
+} HD44780Driver;
+
+/*===========================================================================*/
+/* Driver macros.                                                            */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* External declarations.                                                    */
+/*===========================================================================*/
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void hd44780Init(void);
+void hd44780ObjectInit(HD44780Driver *lcdp);
+void hd44780Start(HD44780Driver *lcdp, const HD44780Config *config);
+void hd44780Stop(HD44780Driver *lcdp);
+void hd44780BacklightOn(HD44780Driver *lcdp);
+void hd44780BacklightOff(HD44780Driver *lcdp);
+void hd44780ClearDisplay(HD44780Driver *lcdp);
+void hd44780ReturnHome(HD44780Driver *lcdp);
+void hd44780SetAddress(HD44780Driver *lcdp, uint8_t add);
+void hd44780WriteString(HD44780Driver *lcdp, uint8_t pos, const char* fmt, ...);
+void hd44780DoDisplayShift(HD44780Driver *lcdp, uint8_t dir);
+#if HD44780_USE_DIMMABLE_BACKLIGHT
+void hd44780SetBacklight(HD44780Driver *lcdp, uint32_t perc);
+void hd44780BacklightFadeOut(HD44780Driver *lcdp);
+void hd44780BacklightFadeIn(HD44780Driver *lcdp);
+#endif /* HD44780_USE_DIMMABLE_BACKLIGHT */
+#ifdef __cplusplus
+}
+#endif
+#endif /* USERLIB_USE_HD44780 */
+
+
+/** @} */
