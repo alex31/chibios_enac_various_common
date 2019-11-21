a1d4091bf3f34fd26eb47bb9bd851069251b8277
Alexandre Bustico <alexandre.bustico@enac.fr>
2019-11-21 17:39:06 +0100

    use standard c++11 noexcept syntax

--- cpp_heap_alloc.hpp
+++ cpp_heap_alloc.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
 #ifdef __cplusplus
-inline void* operator new( size_t size) _GLIBCXX_USE_NOEXCEPT { return malloc_m (size); }
-inline void* operator new[]( size_t size) _GLIBCXX_USE_NOEXCEPT { return malloc_m (size); }
-inline void operator delete( void *p) _GLIBCXX_USE_NOEXCEPT { free_m (p); }
-inline void operator delete[]( void *p) _GLIBCXX_USE_NOEXCEPT  { free_m (p); }
-inline void operator delete( void *p, size_t s) _GLIBCXX_USE_NOEXCEPT { (void) s; free_m (p); }
-inline void operator delete[]( void *p, size_t s) _GLIBCXX_USE_NOEXCEPT  {  (void) s; free_m (p); }
+inline void* operator new( size_t size) noexcept { return malloc_m (size); }
+inline void* operator new[]( size_t size) noexcept { return malloc_m (size); }
+inline void operator delete( void *p) noexcept { free_m (p); }
+inline void operator delete[]( void *p) noexcept  { free_m (p); }
+inline void operator delete( void *p, size_t s) noexcept { (void) s; free_m (p); }
+inline void operator delete[]( void *p, size_t s) noexcept  {  (void) s; free_m (p); }
 #endif
