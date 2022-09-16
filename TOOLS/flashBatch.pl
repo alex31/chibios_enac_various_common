470c3a47a27023a2dea9138dd17c2f30b8cbafc1
Alexandre Bustico <alexandre.bustico@enac.fr>
2022-09-16 19:58:57 +0200

    add a script to flash a batch of numerous MCU with same firmware

---
+++ TOOLS/flashBatch.pl
@@ -0,0 +1,108 @@
+#!/usr/bin/perl
+
+
+use Modern::Perl '2020';
+use feature ':5.30';
+no warnings 'experimental::smartmatch';
+
+use Udev::FFI;
+use Udev::FFI::Devnum qw(:all); # <- import major, minor and makedev
+use Getopt::Long;
+
+use constant NONE => 0;
+use constant STLINK => 1;
+use constant BMP => 2;
+
+sub usage();
+
+my %options;
+my $flashTool = NONE;
+
+GetOptions (\%options, 'stlink', 'bmp', 'help');
+usage() if exists  $options{help};
+usage()  unless scalar (@ARGV == 1);
+
+my $programFile = $ARGV[0];
+die "$programFile not readable\n" unless -r $programFile;
+
+for ($programFile) {
+    when(/.bin$/) {
+	$flashTool = STLINK;
+    }
+    when(/.elf$/) {
+	$flashTool = BMP;
+    }
+}
+
+die "$programFile is of unknow file type (neither .bin or .elf)\n" unless $flashTool != NONE;
+
+
+# get udev library version
+my $udev_version = Udev::FFI::udev_version() or
+    die "Can't get udev library version: $@";
+ 
+ 
+# create Udev::FFI object
+my $udev = Udev::FFI->new() or
+    die "Can't create Udev::FFI object: $@";
+ 
+ 
+# create udev monitor
+my $monitor = $udev->new_monitor() or
+    die "Can't create udev monitor: $@";
+ 
+# add filter to monitor
+unless ($monitor->filter_by_subsystem_devtype('tty')) {
+    warn "Ouch!";
+}
+ 
+# start monitor
+if ($monitor->start()) {
+    for (;;) {
+        # poll devices, now insert or remove your block device
+        my $device = $monitor->poll(); # blocking read
+        my $action = $device->get_action();
+	my $devName = $device->get_sysname();
+
+	my ($devIndex) = $devName =~ m/(\d+)$/;
+	next if ($devIndex % 2) == 0;
+	
+        say sprintf ("%s %s", $action, $device->get_devnode());
+
+	next unless ($action eq 'add');
+	my @com;
+
+	for ($flashTool) {
+	    when (STLINK) {
+		@com = ('/bin/st-flash', 'write', $programFile, '0x08000000');
+	    }
+	    when (BMP) {
+		@com = ("$ENV{HOME}/BIN/bmpflash", $programFile);
+	    }
+	}
+
+	say "Device detected, will flash : " . join(', ', @com);
+	sleep 1;
+	system(@com);
+	say "flash done, you can unplug, hit ctrl-C when finish";
+    }
+}
+
+
+#                                 __ _
+#                                / _` |
+#         _   _   ___     __ _  | (_| |   ___
+#        | | | | / __|   / _` |  \__, |  / _ \
+#        | |_| | \__ \  | (_| |   __/ | |  __/
+#         \__,_| |___/   \__,_|  |___/   \___|
+sub usage ()
+{
+  $0 =~ s|.*/||;
+  my ($x) = ' ' x length($0) ;
+
+  print <<EOL ;
+  usage : $0 file.bin or file.elf
+        :    if file.bin, use st-flash, if file.elf, use black magic probe (bmpflash)
+EOL
+  exit (-1);
+}
