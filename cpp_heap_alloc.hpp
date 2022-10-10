014c2dd6dea9b9b7f0cd6b1883664e0dfe53e861
Alexandre Bustico <alexandre.bustico@enac.fr>
2022-10-10 05:58:50 +0200

    fix allocator stub to avoid error when compiling with exception enabled

--- cpp_heap_alloc.hpp
+++ cpp_heap_alloc.hpp
@@ -9,8 +9,13 @@ extern "C++" {
 //   DebugTrace("new %p", p);
 //   return p;
 // }
-inline void* operator new( size_t size) noexcept {return  malloc_m (size);}
-//inline void* operator new[]( size_t size) noexcept { return malloc_m (size); }
+
+#if defined(__cpp_exceptions) 
+inline void* operator new[]( size_t size)  { return malloc_m (size); }
+#else
+inline void* operator new[]( size_t size) noexcept { return malloc_m (size); }
+#endif
+
 inline void operator delete( void *p) noexcept {
   //  DebugTrace("delete %p", p);
   //  chThdSleepMilliseconds(50);
@@ -19,11 +24,14 @@ inline void operator delete( void *p) noexcept {
 inline void operator delete[]( void *p) noexcept  { free_m (p); }
 inline void operator delete( void *p, size_t s) noexcept { (void) s; free_m (p); }
 inline void operator delete[]( void *p, size_t s) noexcept  {  (void) s; free_m (p); }
+
 }
 
 // to avoid accidental use of stdlib malloc and
 extern "C" {
   inline void *malloc(size_t) {chSysHalt("use of malloc is forbidden"); return (void*) 0;}
 }
-  
+
+
+
 #endif
