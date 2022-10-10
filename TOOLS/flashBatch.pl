4d264180694d3f42e0f4fa19f7c41a824313ad94
Alexandre Bustico <alexandre.bustico@enac.fr>
2022-10-10 05:59:53 +0200

    enhance flashBatch.pl to reconfigure ftdi usb2serial, and to make sound when flash is done

--- TOOLS/flashBatch.pl
+++ TOOLS/flashBatch.pl
@@ -18,24 +18,27 @@ sub usage();
 my %options;
 my $flashTool = NONE;
 
-GetOptions (\%options, 'stlink', 'bmp', 'help');
+GetOptions (\%options, 'stlink', 'bmp', 'ftdi', 'help');
 usage() if exists  $options{help};
-usage()  unless scalar (@ARGV == 1);
+usage()  unless scalar (@ARGV == 1) or exists $options{ftdi};
 
-my $programFile = $ARGV[0];
-die "$programFile not readable\n" unless -r $programFile;
+my $programFile = $ARGV[0] // '';
+die "$programFile not readable\n" unless -r $programFile or exists $options{ftdi};
 
-for ($programFile) {
-    when(/.bin$/) {
-	$flashTool = STLINK;
-    }
-    when(/.elf$/) {
-	$flashTool = BMP;
+unless (exists $options{ftdi}) {
+    for ($programFile) {
+	when(/.bin$/) {
+	    $flashTool = STLINK;
+	}
+	when(/.elf$/) {
+	    $flashTool = BMP;
+	}
     }
 }
 
-die "$programFile is of unknow file type (neither .bin or .elf)\n" unless $flashTool != NONE;
-
+unless (exists $options{ftdi}) {
+    die "$programFile is of unknow file type (neither .bin or .elf)\n" unless $flashTool != NONE;
+}
 
 # get udev library version
 my $udev_version = Udev::FFI::udev_version() or
@@ -57,34 +60,57 @@ unless ($monitor->filter_by_subsystem_devtype('tty')) {
 }
  
 # start monitor
-if ($monitor->start()) {
-    for (;;) {
-        # poll devices, now insert or remove your block device
-        my $device = $monitor->poll(); # blocking read
-        my $action = $device->get_action();
-	my $devName = $device->get_sysname();
-
-	my ($devIndex) = $devName =~ m/(\d+)$/;
-	next if ($devIndex % 2) == 0;
-	
-        say sprintf ("%s %s", $action, $device->get_devnode());
-
-	next unless ($action eq 'add');
-	my @com;
-
-	for ($flashTool) {
-	    when (STLINK) {
-		@com = ('/bin/st-flash', 'write', $programFile, '0x08000000');
+unless (exists $options{ftdi}) {
+    if ($monitor->start()) {
+	for (;;) {
+	    # poll devices, now insert or remove your block device
+	    my $device = $monitor->poll(); # blocking read
+	    my $action = $device->get_action();
+	    my $devName = $device->get_sysname();
+	    
+	    my ($devIndex) = $devName =~ m/(\d+)$/;
+	    next if ($devIndex % 2) == 0;
+	    
+	    say sprintf ("%s %s", $action, $device->get_devnode());
+	    
+	    next unless ($action eq 'add');
+	    my @com;
+	    
+	    for ($flashTool) {
+		when (STLINK) {
+		    @com = ('/bin/st-flash', 'write', $programFile, '0x08000000');
+		}
+		when (BMP) {
+		    @com = ("$ENV{HOME}/BIN/bmpflash", $programFile);
+		}
 	    }
-	    when (BMP) {
-		@com = ("$ENV{HOME}/BIN/bmpflash", $programFile);
-	    }
+	    
+	    say "Device detected, will flash : " . join(', ', @com);
+	    sleep 1;
+	    system(@com);
+	    system('/bin/paplay', '/usr/share/sounds/Oxygen-Sys-App-Positive.ogg');
+	    say "flash done, you can unplug, hit ctrl-C when finish";
 	}
-
-	say "Device detected, will flash : " . join(', ', @com);
-	sleep 1;
-	system(@com);
-	say "flash done, you can unplug, hit ctrl-C when finish";
+    }
+} else {
+    if ($monitor->start()) {
+	for (;;) {
+	    # poll devices, now insert or remove your block device
+	    my $device = $monitor->poll(); # blocking read
+	    my $action = $device->get_action();
+	    my $devName = $device->get_sysname();
+	    
+	    say sprintf ("%s %s", $action, $device->get_devnode());
+	    
+	    next unless ($action eq 'add');
+	    my @com = ('/home/alex/DEV/STM32/CHIBIOS/C21DEV/NUCLEOL432/rfbox/tools' .
+		       '/ftdi_rfbox_eeprom_configure.pl');
+	    
+	    say "Device detected, will flash : " . join(', ', @com);
+	    system(@com);
+	    system('/bin/paplay', '/usr/share/sounds/Oxygen-Sys-App-Positive.ogg');
+	    say "flash done, you can unplug, hit ctrl-C when finish";
+	}
     }
 }
 
@@ -101,7 +127,7 @@ sub usage ()
   my ($x) = ' ' x length($0) ;
 
   print <<EOL ;
-  usage : $0 file.bin or file.elf
+  usage : $0 [-stlink, -bmp, -ftdi] file.bin or file.elf
         :    if file.bin, use st-flash, if file.elf, use black magic probe (bmpflash)
 EOL
   exit (-1);
