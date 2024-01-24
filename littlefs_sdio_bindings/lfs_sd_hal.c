bf068ed0299208eeab832df948ea504dba2f60e6
Alexandre Bustico <alexandre.bustico@enac.fr>
2024-01-24 10:14:00 +0100

    littefs : manage to use attrib to store timestamp in a static way

--- littlefs_sdio_bindings/lfs_sd_hal.c
+++ littlefs_sdio_bindings/lfs_sd_hal.c
@@ -35,21 +35,29 @@
 
 
 /*
-  TODO : remove all static variables in the driver to make it reentrant 
-  : should be able to operate multiple lfs mount in //
-  need a config struct
-  need a driver struct
-  need a function that return lfs from driver to directlu use lfs API
-  
+  TODO :
+
+  * API plus simple et generique pour les attrib avec la fonction set_attrib
+
+
   * optionnaly use hardware assisted CRC(ethernet) calculation
-    ° H7 : DMA+CRC
+    ° F7,H7 : DMA+CRC
 
  */
 
 /*===========================================================================*/
 /* Module local definitions.                                                 */
 /*===========================================================================*/
-#define LFS_LOOKAHEAD_BUFFER_SIZE 16U
+
+int __lfs_sd_read(const struct lfs_config *c, lfs_block_t block,
+		  lfs_off_t off, void *buffer, lfs_size_t size);
+int __lfs_sd_prog(const struct lfs_config *c, lfs_block_t block,
+		  lfs_off_t off, const void *buffer, lfs_size_t size);
+int __lfs_sd_erase(const struct lfs_config *c, lfs_block_t block);
+int __lfs_sd_sync(const struct lfs_config *c);
+int __lfs_sd_lock(const struct lfs_config *c);
+int __lfs_sd_unlock(const struct lfs_config *c);
+
 /*===========================================================================*/
 /* Module exported variables.                                                */
 /*===========================================================================*/
@@ -57,23 +65,14 @@
 /*===========================================================================*/
 /* Module local types.                                                       */
 /*===========================================================================*/
-typedef struct  {
-  SDCDriver *sdcd;
-  size_t blk_num;
-  mutex_t mtx;
-  uint8_t lfs_read_buffer[MMCSD_BLOCK_SIZE];
-  uint8_t lfs_prog_buffer[MMCSD_BLOCK_SIZE];
-  uint8_t lfs_lookahead_buffer[LFS_LOOKAHEAD_BUFFER_SIZE];
-} lfs_sd_context;
 
 /*===========================================================================*/
 /* Module local variables.                                                   */
 /*===========================================================================*/
-static lfs_sd_context  IN_DMA_SECTION_NOINIT(context);
 
-static struct lfs_config lfscfg = {
+static const struct lfs_config lfs_cfg_skl = {
     /* Link to the flash device driver.*/
-  .context            = &context,
+  .context            = nullptr,
   
   /* Block device operations.*/
   .read               = __lfs_sd_read,
@@ -87,60 +86,77 @@ static struct lfs_config lfscfg = {
   .read_size          = MMCSD_BLOCK_SIZE,
   .prog_size          = MMCSD_BLOCK_SIZE,
   .block_size         = MMCSD_BLOCK_SIZE,
-  .block_count        = 0, // dynamically set from SD capacity
+  .block_count        = 0,  // dynamically set from SD capacity
   .block_cycles       = -1, // block device take care of wear leveling
   .cache_size         = MMCSD_BLOCK_SIZE,
   .lookahead_size     = LFS_LOOKAHEAD_BUFFER_SIZE,
-  .read_buffer        = context.lfs_read_buffer,
-  .prog_buffer        = context.lfs_prog_buffer,
-  .lookahead_buffer   = context.lfs_lookahead_buffer,
+  .read_buffer        = nullptr, // dynamically set from LfsSdDriver
+  .prog_buffer        = nullptr, // dynamically set from LfsSdDriver
+  .lookahead_buffer   = nullptr, // dynamically set from LfsSdDriver
   .name_max           = 0,
   .file_max           = 0,
   .attr_max           = 0,
   .metadata_max       = 0
 };
 
-static lfs_t lfs;
+
 
 /*===========================================================================*/
 /* Module local functions.                                                   */
 /*===========================================================================*/
-static bool sdio_start(lfs_sd_context  *ctx);
+static bool sdio_start(LfsSdDriver  *lfsSdd);
 /*===========================================================================*/
 /* Module exported functions.                                                */
 /*===========================================================================*/
 
-lfs_t* lfs_sd_init(SDCDriver* sdcd, lfs_sd_init_t flags)  
+lfs_t* lfsSdStart(LfsSdDriver *lfsSdd, const LfsSdConfig *cfg)
 {
-  context.sdcd = sdcd;
-  chMtxObjectInit(&context.mtx);
+  lfsSdd->cfg = cfg;
+  lfsSdd->lfs_cfg = lfs_cfg_skl;
+  chMtxObjectInit(&lfsSdd->mtx);
   int err;
   
-  if (!sdio_start(&context))
+  if (!sdio_start(lfsSdd))
     return nullptr;
-
-  lfscfg.block_count = context.blk_num;
-  if (flags & LFS_SD_FORMAT) {
-    err = lfs_format(&lfs, &lfscfg);
+  lfsSdd->lfs_cfg.block_count = lfsSdd->blk_num;
+  lfsSdd->lfs_cfg.context = lfsSdd;
+  lfsSdd->lfs_cfg.read_buffer = lfsSdd->lfs_read_buffer;
+  lfsSdd->lfs_cfg.prog_buffer = lfsSdd->lfs_prog_buffer;
+  lfsSdd->lfs_cfg.lookahead_buffer = lfsSdd->lfs_lookahead_buffer;
+  
+  if (cfg->formatDevice == true) {
+    err = lfs_format(&lfsSdd->lfs, &lfsSdd->lfs_cfg);
     if (err < 0) {
       return nullptr;
     }
   }
 
-  err = lfs_mount(&lfs, &lfscfg);
+  err = lfs_mount(&lfsSdd->lfs, &lfsSdd->lfs_cfg);
   if (err < 0) {
     return nullptr;
   }
   
-  return &lfs;
+  return &lfsSdd->lfs;
 }
 
-void   lfs_sd_file_cache_init(lfs_sd_file_cache_t * const cache,
-			      struct lfs_file_config *lfs_file_cfg)
+void   lfsSdFileCacheInit(LfsSdFileCache *cache,
+			  struct lfs_file_config *lfs_file_cfg)
 {
+  struct tm tim;
+  RTCDateTime timespec;
+  
+  rtcGetTime(&RTCD1, &timespec);
+  rtcConvertDateTimeToStructTm(&timespec, &tim, NULL);
+  cache->creationTime = mktime(&tim);
+  cache->lfsAttr = (struct lfs_attr) {
+    .type = LFS_ATTR_CREATION_TIME,
+    .buffer = &cache->creationTime,
+    .size = sizeof(cache->creationTime)
+  };
+
   lfs_file_cfg->buffer = cache->lfs_file_buffer;
-  lfs_file_cfg->attrs = nullptr;
-  lfs_file_cfg->attr_count = 0;
+  lfs_file_cfg->attrs = &cache->lfsAttr;
+  lfs_file_cfg->attr_count = 1;
 }
 
 uint32_t lfs_crc(uint32_t crc, const void *buffer, size_t size)
@@ -204,8 +220,8 @@ int __lfs_sd_read(const struct lfs_config *c, lfs_block_t block,
 {
   chDbgAssert(offset == 0, "offset should be 0");
   chDbgAssert(size == MMCSD_BLOCK_SIZE, "size should be MMCSD_BLOCK_SIZE");
-  lfs_sd_context *ctx = (lfs_sd_context *) c->context;
-  SDCDriver *sdcd = ctx->sdcd;
+  LfsSdDriver *lfsSdd = (LfsSdDriver *) c->context;
+  SDCDriver *sdcd = lfsSdd->cfg->sdcd;
 
   if (!sdioIsConnected())
       return  LFS_ERR_IO;
@@ -224,9 +240,8 @@ int __lfs_sd_prog(const struct lfs_config *c, lfs_block_t block,
 {
   chDbgAssert(offset == 0, "offset should be 0");
   chDbgAssert(size == MMCSD_BLOCK_SIZE, "size should be MMCSD_BLOCK_SIZE");
-
-  lfs_sd_context *ctx = (lfs_sd_context *) c->context;
-  SDCDriver *sdcd = ctx->sdcd;
+  LfsSdDriver *lfsSdd = (LfsSdDriver *) c->context;
+  SDCDriver *sdcd = lfsSdd->cfg->sdcd;
 
   if (!sdioIsConnected())
       return  LFS_ERR_IO;
@@ -258,27 +273,23 @@ int __lfs_sd_erase(const struct lfs_config *, lfs_block_t )
 
 int __lfs_sd_sync(const struct lfs_config *c)
 {
-  lfs_sd_context *ctx = (lfs_sd_context *) c->context;
-  SDCDriver * const sdcd = ctx->sdcd;
+  LfsSdDriver *lfsSdd = (LfsSdDriver *) c->context;
+  SDCDriver *sdcd = lfsSdd->cfg->sdcd;
   const bool sdcStatus = sdcSync(sdcd);
   return sdcStatus == HAL_SUCCESS ? LFS_ERR_OK : LFS_ERR_IO;
 }
 
 int __lfs_sd_lock(const struct lfs_config *c)
 {
-  lfs_sd_context *ctx = (lfs_sd_context *) c->context;
-
-  chMtxLock(&ctx->mtx);
-
+  LfsSdDriver *lfsSdd = (LfsSdDriver *) c->context;
+  chMtxLock(&lfsSdd->mtx);
   return 0;
 }
 
 int __lfs_sd_unlock(const struct lfs_config *c)
 {
-  lfs_sd_context *ctx = (lfs_sd_context *) c->context;
-
-  chMtxUnlock(&ctx->mtx);
-
+  LfsSdDriver *lfsSdd = (LfsSdDriver *) c->context;
+  chMtxUnlock(&lfsSdd->mtx);
   return 0;
 }
 /*
@@ -290,7 +301,7 @@ int __lfs_sd_unlock(const struct lfs_config *c)
 #                |_|     |_|    |_|    \_/    \__,_|  \__|   \___|        
 */
 
-static bool sdio_start(lfs_sd_context *ctx)
+static bool sdio_start(LfsSdDriver  *lfsSdd)
 {
   if  (!sdc_lld_is_card_inserted (nullptr))
     return  false;
@@ -300,10 +311,10 @@ static bool sdio_start(lfs_sd_context *ctx)
       return  false;
   
   BlockDeviceInfo bdi = {};
-  if (blkGetInfo(ctx->sdcd, &bdi)) {
+  if (blkGetInfo(lfsSdd->cfg->sdcd, &bdi)) {
     return false;
   }
-  ctx->blk_num = bdi.blk_num;
+  lfsSdd->blk_num = bdi.blk_num;
 
   chDbgAssert(bdi.blk_size == MMCSD_BLOCK_SIZE,
 	      "device reported non compatible block size");
--- littlefs_sdio_bindings/lfs_sd_hal.h
+++ littlefs_sdio_bindings/lfs_sd_hal.h
@@ -1,35 +1,12 @@
-/*
- ChibiOS - Copyright (C) 2006..2022 Giovanni Di Sirio
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
-/*
- * Parts of this file are:
- *
- * Copyright (c) 2017, Arm Limited. All rights reserved.
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 /**
- * @file    lfs_hal.h
- * @brief   LittleFS-HAL bindings header.
+ * @file    lfs_sd_hal.h
+ * @brief   LittleFS-HAL over SD bindings header.
  *
  * @addtogroup LITTLEFS_BINDINGS
  * @{
  */
 
-#ifndef LFS_WSPI_H
-#define LFS_WSPI_H
+#pragma once
 
 #include "hal.h"
 #include "lfs.h"
@@ -37,6 +14,10 @@
 /*===========================================================================*/
 /* Module constants.                                                         */
 /*===========================================================================*/
+#ifndef  LFS_LOOKAHEAD_BUFFER_SIZE
+#define LFS_LOOKAHEAD_BUFFER_SIZE 16U
+#define LFS_ATTR_CREATION_TIME	  1U
+#endif
 
 /*===========================================================================*/
 /* Module pre-compile time settings.                                         */
@@ -49,10 +30,29 @@
 /*===========================================================================*/
 /* Module data structures and types.                                         */
 /*===========================================================================*/
-typedef enum {LFS_SD_MOUNT=0, LFS_SD_FORMAT=1}  lfs_sd_init_t;
+
+
 typedef struct {
-   uint8_t lfs_file_buffer[MMCSD_BLOCK_SIZE];
-} lfs_sd_file_cache_t;
+  SDCDriver *sdcd;
+  bool	     formatDevice;
+} LfsSdConfig;
+
+typedef struct {
+  const LfsSdConfig *cfg;
+  lfs_t lfs;
+  size_t blk_num;
+  mutex_t mtx;
+  struct lfs_config lfs_cfg;
+  uint8_t lfs_read_buffer[MMCSD_BLOCK_SIZE];
+  uint8_t lfs_prog_buffer[MMCSD_BLOCK_SIZE];
+  uint8_t lfs_lookahead_buffer[LFS_LOOKAHEAD_BUFFER_SIZE];
+} LfsSdDriver;
+
+typedef struct {
+  time_t creationTime;
+  struct lfs_attr lfsAttr;
+  uint8_t lfs_file_buffer[MMCSD_BLOCK_SIZE];
+} LfsSdFileCache;
 /*===========================================================================*/
 /* Module macros.                                                            */
 /*===========================================================================*/
@@ -64,18 +64,9 @@ typedef struct {
 #ifdef __cplusplus
 extern "C" {
 #endif
-  lfs_t* lfs_sd_init(SDCDriver* sdcd, lfs_sd_init_t flags);
-  void   lfs_sd_file_cache_init(lfs_sd_file_cache_t * const cache,
-				struct lfs_file_config *lfs_file_cfg);
-  
-  int __lfs_sd_read(const struct lfs_config *c, lfs_block_t block,
-		    lfs_off_t off, void *buffer, lfs_size_t size);
-  int __lfs_sd_prog(const struct lfs_config *c, lfs_block_t block,
-		    lfs_off_t off, const void *buffer, lfs_size_t size);
-  int __lfs_sd_erase(const struct lfs_config *c, lfs_block_t block);
-  int __lfs_sd_sync(const struct lfs_config *c);
-  int __lfs_sd_lock(const struct lfs_config *c);
-  int __lfs_sd_unlock(const struct lfs_config *c);
+lfs_t* lfsSdStart(LfsSdDriver *lfsSdd, const LfsSdConfig *cfg);
+void   lfsSdFileCacheInit(LfsSdFileCache *cache,
+			  struct lfs_file_config *lfs_file_cfg);
 #ifdef __cplusplus
 }
 #endif
@@ -83,7 +74,5 @@ extern "C" {
 /*===========================================================================*/
 /* Module inline functions.                                                  */
 /*===========================================================================*/
-
-#endif /* LFS_WSPI_H */
 
 /** @} */
