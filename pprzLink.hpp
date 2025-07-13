464e057e5cfdcfd47243bc3f3c5784ff8e0b0043
Alexandre Bustico <alexandre.bustico@enac.fr>
2025-07-13 15:21:14 +0200

    make pprzlink more robust in case of timout on incoming serial message

--- pprzLink.hpp
+++ pprzLink.hpp
@@ -6,6 +6,7 @@
 #include <span>
 #include <array>
 #include <etl/vector.h>
+#include "ch.h"
 
 
 /**
@@ -55,8 +56,8 @@ enum class PprzLinkDecoderState {WAIT_FOR_SYNC, WAIT_FOR_LEN1, WAIT_FOR_LEN2,
 				 PROCESSING_PAYLOAD, WAIT_FOR_CHECKSUM_1, WAIT_FOR_CHECKSUM_2};
 
 using PprzPayload_t = etl::vector<uint8_t, 251>; 
-using PprzMsg_t = etl::vector<uint8_t, 255>; 
-using XbeeMsg_t = etl::vector<uint8_t, 260>; 
+using PprzMsg_t = etl::vector<uint8_t, PprzPayload_t::MAX_SIZE + 4>; 
+using XbeeMsg_t = etl::vector<uint8_t, PprzPayload_t::MAX_SIZE + 9>; 
 
 
 template <auto F>
@@ -131,19 +132,83 @@ union XbeeHeader {
 
   uint8_t raw[5];
 };
-
 static_assert(sizeof(XbeeHeader) == 5);
 
-template<PprzPolicy P, auto SENDMSG_f, auto TRAPERR_f = [](uint32_t, uint32_t) {}>
-requires SendMsgConcept<SENDMSG_f> && TrapErrorConcept<TRAPERR_f>
-class PprzDecoder {
-public:
+
+/*
+     C PPRZ_DATA
+      0 SOURCE (~sender_ID)
+      1 DESTINATION (can be a broadcast ID)
+      2 CLASS/COMPONENT
+        bits 0-3: 16 class ID
+        bits 4-7: 16 component ID
+      3 MSG_ID
+      4 MSG_PAYLOAD
+      . DATA (messages.xml)
+*/
+union PprzHeader {
+  struct {
+    uint8_t source;
+    uint8_t destination;
+    uint8_t classId:4; // low nibble
+    uint8_t componentId:4; // high nibble
+    uint8_t msgId;
+  } __attribute__((packed));
+   uint8_t raw[4];
+
+  PprzHeader() = default;
+  PprzHeader(const PprzHeader&) = default;
+  PprzHeader(PprzHeader&&) = default;
+  PprzHeader& operator=(const PprzHeader&) = default;
+  PprzHeader& operator=(PprzHeader&&) = default;
+  ~PprzHeader() = default;
+
+  template<typename T>
+  requires (sizeof(T) == 1 && std::is_trivially_copyable_v<T>)
+  PprzHeader(std::span<const T> bytes)
+  {
+    this->bitCopy(bytes);
+  }
+
+  template<typename T>
+  requires (sizeof(T) == 1 && std::is_trivially_copyable_v<T>)
+  void bitCopy(std::span<const T> bytes)
+  {
+    chDbgAssert(bytes.size() >= sizeof(PprzHeader), "Buffer too small for PprzHeader");
+    memcpy(this, bytes.data(), sizeof(PprzHeader));
+  }
+
+} __attribute__((packed));
+static_assert(sizeof(PprzHeader) == 4);
+
+constexpr auto default_sendmsg = [](PprzPolicy, const std::span<uint8_t>) {};
+constexpr auto default_traperr = [](uint32_t, uint32_t) {};
+
+constexpr unsigned long long operator""_ms(unsigned long long ms) {
+    return ms;
+}
+
+namespace {
+  /**
+   * @brief Computes the CRC-16 of the provided buffer (Fletcher16).
+   */
+  uint16_t fletcher16(std::span<const uint8_t> data);
+  
+  /**
+   * @brief Computes the CRC-8 of the provided buffer.
+   */
+  uint8_t chksum8(std::span<const uint8_t> data);
+}
+
+namespace PprzEncoder {
   /**
    * @brief Generates a PPRZLink message from the given payload.
    * @param payload Data to encapsulate
    * @return Complete PPRZLink message (header + payload + CRC)
    */
-  static PprzMsg_t genPprzMsg(const PprzPayload_t &payload);
+  PprzMsg_t genPprzMsg(const PprzPayload_t &payload);
+  void genPprzMsg(PprzMsg_t& msg, const PprzHeader &ppHeader,
+			 const std::span<const uint8_t> &payload);
 
   /**
    * @brief Generates an XBee API message from the given header and payload.
@@ -151,7 +216,18 @@ public:
    * @param payload  Payload to send
    * @return Complete XBee message (header + payload + CRC)
    */
-  static XbeeMsg_t genXbeeMsg(const XbeeHeader &xbHeader, const PprzPayload_t &payload);
+  XbeeMsg_t genXbeeMsg(const XbeeHeader &xbHeader, const PprzPayload_t &payload);
+  void genXbeeMsg(XbeeMsg_t& msg, const XbeeHeader &xbHeader,
+		  const PprzHeader &ppHeader, const std::span<const uint8_t> &payload);
+}
+
+template<PprzPolicy P,
+	 auto TIMEOUT_MS = 1000,
+	 auto SENDMSG_f = default_sendmsg,
+         auto TRAPERR_f = default_traperr>
+requires SendMsgConcept<SENDMSG_f> && TrapErrorConcept<TRAPERR_f>
+class PprzDecoder {
+public:
 
   /**
    * @brief Feeds the parser with a buffer of bytes (from stream DMA/IRQ).
@@ -165,6 +241,7 @@ public:
    */
   std::pair<uint32_t, uint32_t> getStat() {return {valid, invalid};}
 
+  
 private:
   /**
    * @brief Buffer for accumulating the payload of the current message.
@@ -197,80 +274,32 @@ private:
   PprzLinkDecoderState state = PprzLinkDecoderState::WAIT_FOR_SYNC;
 
   /**
-   * @brief Computes the CRC-16 of the provided buffer (Fletcher16).
+   * @brief Timestamp of the last received data.
    */
-  static uint16_t fletcher16(std::span<const uint8_t> data);
-
+  systime_t last_activity = 0;
+  
   /**
-   * @brief Computes the CRC-8 of the provided buffer.
-   */
-  static uint8_t chksum8(std::span<const uint8_t> data);
-
-  /**
-   * @brief Resets the parser to the initial state (optional, add if you centralize resets).
+   * @brief Resets the parser to the initial state.
    */
   void reset() {
     state = PprzLinkDecoderState::WAIT_FOR_SYNC;
     payload.clear();
   }
+
+  /**
+   * @brief Checks for a timeout since the last received byte.
+   * If a timeout occurs, it calls the error callback and resets the parser state.
+   */
+  void check_timeout() {
+    if (state != PprzLinkDecoderState::WAIT_FOR_SYNC &&
+        chVTTimeElapsedSinceX(last_activity) >= TIME_MS2I(TIMEOUT_MS)) {
+      TRAPERR_f(valid, ++invalid);
+      reset();
+    }
+    last_activity = chVTGetSystemTimeX();
+  }
 };
 
-
-template<PprzPolicy P, auto SENDMSG_f, auto TRAPERR_f>
-requires SendMsgConcept<SENDMSG_f> && TrapErrorConcept<TRAPERR_f>
-PprzMsg_t PprzDecoder<P, SENDMSG_f, TRAPERR_f>::genPprzMsg(const PprzPayload_t &payload)
-{
-  static_assert(P == PprzPolicy::PPRZ);
-  PprzMsg_t msg;
-  const uint8_t header[] = {0x99, static_cast<uint8_t>(payload.size() + 4U)};
-  msg.insert(msg.begin(), std::begin(header), std::end(header));
-  msg.insert(msg.end(), payload.begin(), payload.end());
-  const uint16_t chksum = fletcher16(std::span<const uint8_t>(msg.begin()+1, msg.end()));
-  const uint8_t tail[] = { static_cast<uint8_t>(chksum & 0xFF),
-			   static_cast<uint8_t>((chksum >> 8) & 0xFF) };
-  msg.insert(msg.end(), std::begin(tail), std::end(tail));
-  
-  return msg;
-}
-
-template<PprzPolicy P, auto SENDMSG_f, auto TRAPERR_f>
-requires SendMsgConcept<SENDMSG_f> && TrapErrorConcept<TRAPERR_f>
-XbeeMsg_t PprzDecoder<P, SENDMSG_f, TRAPERR_f>::genXbeeMsg(const XbeeHeader &xbHeader,
-							   const PprzPayload_t &payload)
-{
-  /*
-    A XBEE_START (0x7E)
-    B LENGTH_MSB (D->D)
-    C LENGTH_LSB
-    D XBEE_PAYLOAD
-    0 XBEE_TX16 (0x01) / XBEE_RX16 (0x81)
-    1 FRAME_ID (0)     / SRC_ID_MSB
-    2 DEST_ID_MSB      / SRC_ID_LSB
-    3 DEST_ID_LSB      / XBEE_RSSI
-    4 TX16_OPTIONS (0) / RX16_OPTIONS
-    5 PPRZ_DATA
-    0 SOURCE (~sender_ID)
-    1 DESTINATION (can be a broadcast ID)
-    2 CLASS/COMPONENT
-    bits 0-3: 16 class ID
-    bits 4-7: 16 component ID
-    3 MSG_ID
-    4 MSG_PAYLOAD
-    . DATA (messages.xml)
-  */
-  static_assert(P == PprzPolicy::XBEE_API);
-  XbeeMsg_t msg;
-  const uint16_t len = payload.size() + sizeof(XbeeHeader);
-  const uint8_t header[] = {0x7e, static_cast<uint8_t>(len >> 8),
-			    static_cast<uint8_t>(len & 0xff)};
-  msg.insert(msg.begin(), std::begin(header), std::end(header));
-  msg.insert(msg.end(), std::begin(xbHeader.raw), std::end(xbHeader.raw));
-  msg.insert(msg.end(), payload.begin(), payload.end());
-  const uint8_t chksum = chksum8(std::span<const uint8_t>(msg.begin()+3, msg.end()));
-  msg.insert(msg.end(), chksum);
-  
-  return msg;
-}
 
 
 /**
@@ -300,11 +329,17 @@ XbeeMsg_t PprzDecoder<P, SENDMSG_f, TRAPERR_f>::genXbeeMsg(const XbeeHeader &xbH
  *
  * Any parsing error (invalid size, CRC mismatch, overflow) triggers a reset to WAIT_FOR_SYNC.
  */
-template<PprzPolicy P, auto SENDMSG_f, auto TRAPERR_f>
+template<PprzPolicy P, auto TIMEOUT_MS, auto SENDMSG_f, auto TRAPERR_f>
 requires SendMsgConcept<SENDMSG_f> && TrapErrorConcept<TRAPERR_f>
-void PprzDecoder<P, SENDMSG_f, TRAPERR_f>::feed(std::span<const uint8_t> data)
+void PprzDecoder<P, TIMEOUT_MS, SENDMSG_f, TRAPERR_f>::feed(std::span<const uint8_t> data)
 {
   static constexpr uint8_t syncBeacon = P == PprzPolicy::PPRZ ? 0x99 : 0x7E;
+
+  // if this bunch of bytes arrive long after previous one, it's not the end of a
+  // previous message, but the beginning of a new one, so we reset the state machine not to miss
+  // the start of message.
+  check_timeout();
+  
   while (!data.empty()) {
     switch (state) {
     case PprzLinkDecoderState::WAIT_FOR_SYNC :
@@ -343,7 +378,8 @@ void PprzDecoder<P, SENDMSG_f, TRAPERR_f>::feed(std::span<const uint8_t> data)
       if constexpr (P == PprzPolicy::XBEE_API) {
 	remaining |= b; // LSB Last
       } else {
-	chSysHalt("WAIT_FOR_LEN2 state incompatible with PPRZ format");
+	TRAPERR_f(valid, ++invalid);
+	reset();
       }
       // DebugTrace ("LEN = %u", remaining);
       state = PprzLinkDecoderState::PROCESSING_PAYLOAD;
@@ -395,13 +431,14 @@ void PprzDecoder<P, SENDMSG_f, TRAPERR_f>::feed(std::span<const uint8_t> data)
       const auto b = data.front();
       data = data.subspan(1);
       if constexpr (P == PprzPolicy::XBEE_API) {
-	chSysHalt("WAIT_FOR_CHECKSUM_2 state incompatible with PPRZ format");
+	TRAPERR_f(valid, ++invalid);
+	reset();
       }
       chksum = chksum | (b << 8);
       const uint16_t calculatedChksum = fletcher16(payload);
       if (calculatedChksum == chksum) {
 	++valid;
-	SENDMSG_f(P, std::span<const uint8_t>(payload.begin() + 1, payload.end()));
+	SENDMSG_f(P, std::span(payload.begin() + 1, payload.end()));
       } else {
 	TRAPERR_f(valid, ++invalid);
       }
@@ -416,28 +453,26 @@ void PprzDecoder<P, SENDMSG_f, TRAPERR_f>::feed(std::span<const uint8_t> data)
   simplified algo : overflow if len > 5802
   which won't affect us, hope we'll use hardware crc32 when we'll have messages this long
 */
-template<PprzPolicy P, auto SENDMSG_f, auto TRAPERR_f>
-requires SendMsgConcept<SENDMSG_f> && TrapErrorConcept<TRAPERR_f>
-uint16_t PprzDecoder<P, SENDMSG_f, TRAPERR_f>::fletcher16(std::span<const uint8_t> data)
-{
-  uint32_t c0 = 0, c1 = 0;
-  for (const uint8_t b : data) { 
-    c0 += b;
-    c1 += c0;
+namespace {
+  uint16_t fletcher16(std::span<const uint8_t> data)
+  {
+    uint32_t c0 = 0, c1 = 0;
+    for (const uint8_t b : data) { 
+      c0 += b;
+      c1 += c0;
+    }
+    c0 %= 0xff;
+    c1 %= 0xff;
+    return (c1 << 8) | c0;
   }
-  c0 %= 0xff;
-  c1 %= 0xff;
-  return (c1 << 8) | c0;
-}
+  
 
-
-template<PprzPolicy P, auto SENDMSG_f, auto TRAPERR_f>
-requires SendMsgConcept<SENDMSG_f> && TrapErrorConcept<TRAPERR_f>
-uint8_t PprzDecoder<P, SENDMSG_f, TRAPERR_f>::chksum8(std::span<const uint8_t> data)
-{
-  uint8_t sum = 0;
-  for (const auto b : data) {
-    sum += b;
+  uint8_t chksum8(std::span<const uint8_t> data)
+  {
+    uint8_t sum = 0;
+    for (const auto b : data) {
+      sum += b;
+    }
+    return 0xFF - sum;
   }
-  return sum;
 }
